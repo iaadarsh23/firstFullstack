38f53d82fec48a2dd712446cc3492fd9627f77ff
Adarsh <adarshtrip2306@gmail.com>
2025-04-26 21:45:41 +0530

    settting the axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-	return <h1>hello adarsh</h1>;
+	const [jokes, setJokes] = useState([]);
+	return (
+		<>
+			<h1>here are my jokes</h1>
+			<p>JOKES: {jokes.length}</p>
+
+			{jokes.map((joke, index) => {
+				<div key={joke.id}>
+					<h2>{joke.title}</h2>
+					<h2>{joke.content}</h2>
+				</div>;
+			})}
+		</>
+	);
 }
 
 export default App;
